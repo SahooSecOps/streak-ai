a7fc4f1ffa8353bdef376c4423ad5fdb054f7f68
bibhu <bibhu>
2025-06-26 03:39:49 +0000

    update test script

--- test_api.sh
+++ test_api.sh
@@ -1,22 +1,38 @@
-process=$(docker ps | grep streakai | awk '{print $1}')
+process=$(docker ps | grep streakai | awk '{print $1}' | wc -l)
 if [ "$process" -eq 1 ]
+then
 	docker rm -f streakai
 fi
 docker run -d -p 5000:5000 --name streakai localhost:5001/streakai:r1
+sleep 5
 while true;
 do
-	process=$(docker ps  | grep streakai | awk '{print $1}')
+	process=$(docker ps  | grep streakai | awk '{print $1}' | wc -l)
 	if [ "$process" -eq 1 ]
+	then
 		echo "service is up and running"
 		break
 	else
 		echo "service is not running"
 		count=$((count + 1))
 		if [ $count -gt 10  ]
+		then
 			echo "not able to run container"
 			break
 		fi
 	fi
 done
-curl "http://localhost:5000/add?num1=3&num2=5"
+response_code=$(timeout 10 curl -L -s -o /dev/null -w "%{http_code}" \
+	-X GET \
+	"http://localhost:5000/add?num1=3&num2=5"
+	)
+echo $response_code
+if [ $response_code -eq 200  ];
+then
+	echo "api is working"
+	exit 0
+else
+	echo "api is not working"
+	exit 1
+fi
 
